ed96ca62b816c8441fefd672c2f5bbade3b174d7
Ted Armstrong <theodorecarmstrong@gmail.com>
2026-02-10 15:38:49 -0500

    feat: adds the workflow builder service to build a DAG from work orders

---
+++ prompts/workflow-builder-service.md
@@ -0,0 +1,7 @@
+# Prompts
+
+## Add Unit Tests
+
+Implement unit tests for the WorkflowBuilderService in workflow-builder.service.spec.ts . The unit tests should cover many test cases involving building Directed Acyclic Graphs those valid and invalid.
+
+The input structure should resemble a valid collection of Work Orders for an ERP, as outlined in @BE-technical-test.md 
--- src/reflow/reflow.service.ts
+++ src/reflow/reflow.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { ReflowDocument, WorkOrderDocument, WorkCenterDocument, ManufacturingOrderDocument, ReflowResult } from "./types";
+import { WorkflowBuilderService } from "./workflow-builder.service";
+
+type ReflowServiceInput = {
+    workOrders: WorkOrderDocument[];
+    workCenters: WorkCenterDocument[];
+    manufacturingOrders: ManufacturingOrderDocument[];
+}
 
 @Injectable()
-export class ReflowService {}
+export class ReflowService {
+    constructor() {}
+
+    reflow(input: ReflowServiceInput): ReflowResult {
+        return {
+            updatedWorkOrders: [],
+            changes: [],
+            explanation: [],
+        }
+    }
+}
